7efe864bd9bd75340e92573b2e4bf516675957d7
samonuall <samonuall@gmail.com>
2023-11-05 15:03:25 -0500

    add research carousel component start

---
+++ components/ResearchCarousel.tsx
@@ -0,0 +1,4 @@
+"use client"
+import { useState, useEffect } from "react"
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import 'react-horizontal-scrolling-menu/dist/styles.css';
