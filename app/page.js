4c4393257f147b0b65650e90053c73b7ff5e6658
samonuall <samonuall@gmail.com>
2023-11-06 23:33:58 -0500

    add research carousel demo

--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 import { Navbar } from "@components/Navbar"
+import { ResearchCarousel } from "@components/ResearchCarousel"
 import Image from "next/image"
 import styles from "@styles/home.module.css"
 
 export default function Home() {
+  let data = Array(20).fill(["Hello", "/temporary-header.png"]);
+
   return (
-    <> 
-      <div className={styles.tempHeaderImage}>
-        <Image src={"/temporary-header.png"} width="1080" height="240"></Image>
-      </div>
-      <Navbar></Navbar>
-    </>
-  )
+      <> 
+        <div className={styles.tempHeaderImage}>
+          <Image src={"/temporary-header.png"} width="1080" height="240"></Image>
+        </div>
+        <Navbar></Navbar>
+        
+        <ResearchCarousel data={data}/>
+      </>
+    );
 }
