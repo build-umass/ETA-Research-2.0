180449cc400f9e247ac36e0c20986b043e5eceb4
samonuall <samonuall@gmail.com>
2023-11-06 23:34:40 -0500

    change to js

--- components/ResearchCarousel.tsx
+++
@@ -1,4 +0,0 @@
-"use client"
-import { useState, useEffect, useContext } from "react"
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import 'react-horizontal-scrolling-menu/dist/styles.css';
