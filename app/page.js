991c6fdb26200d4aebf92d39d51e3d68811cb384
Neha <nehaka02@gmail.com>
2023-11-17 12:51:44 -0500

    Fetching from contentful api

--- app/page.js
+++ app/page.js
@@ -1,6 +1,22 @@
 import { Navbar } from "@components/Navbar"
+import { createClient } from "contentful"
 import Image from "next/image"
 import styles from "@styles/home.module.css"
+
+
+async function getData() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  })
+  const res = await client.getEntries({ content_type: "researchItem" })
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+  console.log(res.json())
+  return res.json()
+}
 
 export default function Home() {
   return (
