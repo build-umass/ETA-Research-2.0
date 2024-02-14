53c31885ba79e4760ce2e11d55193fc1df341945
Roman Pisani <romanp2929@gmail.com>
2024-02-13 23:00:42 -0500

    Header and footer, and small refactor

--- app/page.js
+++ app/page.js
@@ -1,18 +1,11 @@
-import { Navbar } from "@components/Navbar"
-import Image from "next/image"
 import styles from "@styles/home.module.css"
-import Header from "@components/Header"
-import Footer from "@components/Footer"
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
 
   return (
     <> 
-      <Header />
-      <Navbar />
       <div className={styles.spacingDiv} />
-      <Footer />
     </>
   )
 }
