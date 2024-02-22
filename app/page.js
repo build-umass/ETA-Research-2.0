53991e84a92304c9d00ccee9899ecfec972a37cb
Neha <nehaka02@gmail.com>
2024-02-21 22:25:58 -0500

    Added image carousel on home page

--- app/page.js
+++ app/page.js
@@ -3,15 +3,27 @@ import Image from "next/image"
 import styles from "@styles/home.module.css"
 import Header from "@components/Header"
 import Footer from "@components/Footer"
+import ImageSlider from "@components/ImageSlider"
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
-
+  const slides = [
+    {url: "http://localhost:3000/students.jpeg", title: "Students"},
+    {url: "http://localhost:3000/BridgetteTeam.JPEG", title: "Team"}
+  ]
+  const containerStyles = {
+    width: "750px",
+    height: "420px",
+    margin: "0 auto",
+  };
   return (
     <> 
       <Header />
       <Navbar />
       <div className={styles.spacingDiv} />
+      <div style={containerStyles}>
+        <ImageSlider slides={slides} />
+      </div>
       <Footer />
     </>
   )
