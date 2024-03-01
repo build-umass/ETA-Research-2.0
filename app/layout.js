e39e86bb4e29a19443950fa09fd5a400490b495c
Roman Pisani <romanp2929@gmail.com>
2024-03-01 17:19:19 -0500

    temporary auth for showcase and deployment purposes

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,63 @@
+"use client"
 import "@styles/globals.css"
 import Header from "@components/Header"
 import Footer from "@components/Footer"
 import { Navbar } from "@components/Navbar"
+import { useEffect, useState, useRef } from "react"
+import { config } from "dotenv"
 
-export const metadata = {
-  title: 'ETA Research',
-  description: 'Research done by Equitable Transitions to Adulthood',
-  author: 'BUILD UMass',
-  keywords: ['ETA Research', 'Equitable Transitions', 'Adulthood', 'research'],
-  image: '/temporary-header.png', 
-  siteName: 'ETA Research'
-};
-
-
-export default function RootLayout({ children }) {
+// export const metadata = {
+  //   title: 'ETA Research',
+  //   description: 'Research done by Equitable Transitions to Adulthood',
+  //   author: 'BUILD UMass',
+  //   keywords: ['ETA Research', 'Equitable Transitions', 'Adulthood', 'research'],
+  //   image: '/temporary-header.png', 
+  //   siteName: 'ETA Research'
+  // };
   
+export default function RootLayout({ children }) {
+
+  const [ auth, setAuth ] = useState(false)
+  useEffect(() => {
+    config()
+    if (localStorage.getItem("auth") === process.env.NEXT_PUBLIC_AUTH) {
+      console.log(localStorage.getItem("auth"))
+      setAuth(true)
+    } else {
+      console.log(localStorage.getItem("auth"))
+      console.log(process.env.NEXT_PUBLIC_AUTH)
+    }
+    console.log(auth)
+  }, [])
+
+  const handleAuthSubmit = () => {
+    if (inputRef.current.value === process.env.NEXT_PUBLIC_AUTH) {
+      setAuth(true)
+      localStorage.setItem("auth", inputRef.current.value)
+    }
+  }
+
+  const inputRef = useRef(null)
+
   return (
     <html lang="en">
       <body>
-        <Navbar />
-        <div className="mainContainer">
-          <Header />
-          {children}
-        </div>
-        <Footer />
+        { auth ? 
+          <>
+            <Navbar />
+            <div className="mainContainer">
+              <Header />
+              {children}
+            </div>
+            <Footer /> 
+          </>
+        :
+          <>
+            <input type="text" ref={inputRef}></input>
+            <button onClick={handleAuthSubmit}>Submit</button>
+          </>
+        }
+          
       </body>
     </html>
   )
--- services/contentfulUtils.js
+++ services/contentfulUtils.js
@@ -2,50 +2,50 @@
 import { createClient, getAsset } from 'contentful';
 
 const client = createClient({
-    space: "72na10vz3n2a",
-    accessToken: "dW-vL_XZ7d0NVV-1XZcwLf5srRlwxN8h71e5W24WLaE"
+  space: "72na10vz3n2a",
+  accessToken: "dW-vL_XZ7d0NVV-1XZcwLf5srRlwxN8h71e5W24WLaE"
 });
 
 
 async function getData(content_type) {
-    const response = await client.getEntries({content_type: content_type});
-    return response.items;
+  const response = await client.getEntries({ content_type: content_type });
+  return response.items;
 }
 
 export function getStudentData() {
-    const result = getData("studentItem");
+  const result = getData("studentItem");
 
-    const studentData = result.then(data => {
-        // do error handling here
-        return data;
-    });
+  const studentData = result.then(data => {
+    // do error handling here
+    return data;
+  });
 
-    // if there is an error, it will be caught here
-    studentData.catch(err => {
-        console.error("An error occurred:", err);
-        throw err;
-    });
+  // if there is an error, it will be caught here
+  studentData.catch(err => {
+    console.error("An error occurred:", err);
+    throw err;
+  });
 
-    
-    return studentData;
+
+  return studentData;
 }
 
 export function getResearchData() {
-    const result = getData("researchItem");
+  const result = getData("researchItem");
 
-    const researchData = result.then(data => {
-        // do error handling here
-        return data;
-    });
+  const researchData = result.then(data => {
+    // do error handling here
+    return data;
+  });
 
-    // if there is an error, it will be caught here
-    researchData.catch(err => {
-        console.error("An error occurred:", err);
-        throw err;
-    });
+  // if there is an error, it will be caught here
+  researchData.catch(err => {
+    console.error("An error occurred:", err);
+    throw err;
+  });
 
-    
-    return researchData;
+
+  return researchData;
 }
 
 export default getData;
