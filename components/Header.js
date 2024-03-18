b4b6e49a2e6a185705dbf107d6b7219a35b480b4
Roman Pisani <romanp2929@gmail.com>
2024-03-18 17:08:14 -0400

    updated header image '

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,8 @@ import styles from '@styles/header.module.css'
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
-      <img className={styles.headerImageSmall} src={"/HeaderImageSmall.jpg"} />
-      <img className={styles.headerImageLarge} src={"/HeaderImageLarge.jpg"} />
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>Equitable Transitions to Adulthood</h1>
-        <h2 className={styles.subtitle}>Research Center</h2>
-      </div>
-      
+      <img className={styles.headerImageSmall} src={"/HeaderImageSmall.png"} />
+      <img className={styles.headerImageLarge} src={"/HeaderImageLarge.png"} />
     </div>
   )
 }
